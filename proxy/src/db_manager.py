35c06d379fd0764ee121d061925710f023c7f407
oruamdalo <dalo.mauro.25@gmail.com>
2023-04-17 17:39:08 +0200

    Fixed database singleton for thread saftey. Added automatic function detection in modules

--- proxy/src/db_manager.py
+++ proxy/src/db_manager.py
@@ -1,12 +1,20 @@
-import pymongo
 from src.constants import DB_URL
+import threading
+import pymongo
 
-class DBManager(object):
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(DBManager, cls).__new__(cls)
-        return cls.instance
+lock = threading.Lock()
 
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            with lock:
+                if cls not in cls._instances:
+                    cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+    
+class DBManager(metaclass=Singleton):
     def __init__(self):
         self.client = pymongo.MongoClient(DB_URL)
         self.db = self.client.db
